50495ea5e9a080c1c71d413802b694c7feaa28d9
Roland Vachter <roland.vachter@ft.com>
2014-07-29 12:01:43 +0300

    commentingSettings renamed to commentingSettingsLabel.

--- src/javascripts/i18n.js
+++ src/javascripts/i18n.js
@@ -6,7 +6,7 @@ exports.texts = {
     unavailable: 'It seems we\'re having a problem loading comments for you. Apologies. Hopefully it\'s temporary, but if you continue to experience this difficulty, please contact <a href="mailto:help@ft.com">help@ft.com</a>.',
     changePseudonymError: 'Pseudonym creation is temporarily unavailable, please try again later.',
     genericError: 'An error occured, please try again later.',
-    commentingSettings: 'Commenting settings'
+    commentingSettingsLabel: 'Commenting settings'
 };
 
 /**
