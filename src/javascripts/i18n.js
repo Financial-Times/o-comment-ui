5602c5c7768e9c29b2e76555bbab7fdafae12383
Roland Vachter <roland.vachter@ft.com>
2014-08-12 16:54:42 +0300

    Pseudonym blank case message added.

--- src/javascripts/i18n.js
+++ src/javascripts/i18n.js
@@ -5,6 +5,7 @@
 exports.texts = {
     unavailable: 'It seems we\'re having a problem loading comments for you. Apologies. Hopefully it\'s temporary, but if you continue to experience this difficulty, please contact <a href="mailto:help@ft.com">help@ft.com</a>.',
     changePseudonymError: 'Pseudonym creation is temporarily unavailable, please try again later.',
+    changePseudonymBlankError: 'Pseudonym is blank.',
     genericError: 'An error occured, please try again later.',
     commentingSettingsLabel: 'Commenting settings'
 };
