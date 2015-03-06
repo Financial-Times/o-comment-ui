cd734f18e22b9596863e390611bba251d3df6b0c
roland.vachter <roland.vachter@ft.com>
2015-03-06 11:44:37 +0200

    Fix instantiating overlays each time.

--- src/javascripts/templates.js
+++ src/javascripts/templates.js
@@ -7,8 +7,8 @@ var hogan = require('hogan');
  * @type {Object}
  */
 module.exports = {
-    unavailableTemplate: hogan.compile(requireText('../templates/unavailable.ms')),
-    termsAndGuidelinesTemplate: hogan.compile(requireText('../templates/termsAndGuidelines.ms')),
-    clearLine: hogan.compile(requireText('../templates/clearLine.ms')),
-    commentingSettingsLink: hogan.compile(requireText('../templates/commentingSettingsLink.ms'))
+	unavailableTemplate: hogan.compile(requireText('../templates/unavailable.ms')),
+	termsAndGuidelinesTemplate: hogan.compile(requireText('../templates/termsAndGuidelines.ms')),
+	clearLine: hogan.compile(requireText('../templates/clearLine.ms')),
+	commentingSettingsLink: hogan.compile(requireText('../templates/commentingSettingsLink.ms'))
 };
