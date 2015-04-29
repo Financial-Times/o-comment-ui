76e7129a7ddd1f7019d0097bf17da5e32e818eeb
Roland Vachter <roland.vachter@iquestgroup.com>
2015-04-29 10:58:33 +0300

    CAE-322: Expose utils to the public API.

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ var oCommentUtilities = require('o-comment-utilities');
 exports.Widget = require('./src/javascripts/Widget.js');
 exports.WidgetUi = require('./src/javascripts/WidgetUi.js');
 exports.userDialogs = require('./src/javascripts/userDialogs.js');
+exports.utils = require('./src/javascripts/utils.js');
 
 exports.i18n = require('./src/javascripts/i18n.js');
 exports.templates = require('./src/javascripts/templates.js');
