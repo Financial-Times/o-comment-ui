24683a215de86994e1dc76bb4108516829a98819
Roland Vachter <roland.vachter@iquestgroup.com>
2015-11-11 15:02:36 +0200

    Auth unavailable template added.

--- src/javascripts/templates.js
+++ src/javascripts/templates.js
@@ -6,6 +6,7 @@ const hogan = require('hogan');
  */
 module.exports = {
 	unavailableTemplate: hogan.compile(requireText('../templates/unavailable.ms')),
+	authUnavailableTemplate: hogan.compile(requireText('../templates/authUnavailable.ms')),
 	termsAndGuidelinesTemplate: hogan.compile(requireText('../templates/termsAndGuidelines.ms')),
 	clearLine: hogan.compile(requireText('../templates/clearLine.ms')),
 	commentingSettingsLink: hogan.compile(requireText('../templates/commentingSettingsLink.ms')),
